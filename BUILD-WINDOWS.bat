df671840e4c9aea74094c643699fdc396b63c059
Fortnit3340 <lekamledavid@gmail.com>
2026-09-20 12:48:49 +0200

    Delete BUILD-WINDOWS.bat

--- BUILD-WINDOWS.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-where node >nul 2>nul || (echo Node.js is required. & pause & exit /b 1)
-call npm install
-call npm run dist
-echo Installer is in the release folder.
-pause
